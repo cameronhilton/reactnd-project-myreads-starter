f2ed951aaf5685fad2262e34e90401c9fd30deae
Cameron Hilton <cameronhilton@gmail.com>
2020-05-04 14:43:35 -0700

    change default values of Book component

--- src/Book.js
+++ src/Book.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Book extends Component {
-  render() {
-    const { authors, imageLinks, shelf, title } = this.props.book
+function Book(props) {
+  const { authors, imageLinks, shelf, title } = props.book
 
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div
-            className="book-cover"
-            style={{ width: 128, height: 193,
-                     backgroundImage: `url(${imageLinks && imageLinks.thumbnail})` }}>
-          </div>
-          <div className="book-shelf-changer">
-            <select
-              defaultValue={'move'}
-              onChange={this.props.updateShelf.bind(null, this.props.book)}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading" disabled={shelf === 'currentlyReading'}>Currently Reading</option>
-              <option value="wantToRead" disabled={shelf === 'wantToRead'}>Want to Read</option>
-              <option value="read" disabled={shelf === 'read'}>Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div
+          className="book-cover"
+          style={{ width: 128, height: 193,
+                    backgroundImage: `url(${imageLinks && imageLinks.thumbnail})` }}>
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">
-          {authors && authors.map((author, idx) => (
-            <span key={author}>
-              {author}
-              {idx < authors.length - 1 && <span style={{ paddingRight: '3px'}}>,</span>}
-            </span>
-          ))}
+        <div className="book-shelf-changer">
+          <select
+            defaultValue={typeof shelf !== 'undefined' ? shelf : 'none'}
+            onChange={props.updateShelf.bind(null, props.book)}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
       </div>
-    )
-  }
+      <div className="book-title">{title}</div>
+      <div className="book-authors">
+        {authors && authors.map((author, idx) => (
+          <span key={author}>
+            {author}
+            {idx < authors.length - 1 && <span style={{ paddingRight: '3px'}}>,</span>}
+          </span>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Book
